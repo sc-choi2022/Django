1bf8c7ee45c9cd2389fd2a4139d7322c94d393e7
최수연 <martin0125@naver.com>
2022-04-07 14:37:13 +0900

    create

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .forms import ArticleForm
 
-# Create your views here.
+def create(request):
+    if request.method == 'POST':
+        # create
+        form = ArticleForm(request.POST)
+        # validation
+        if form.is_valid():
+            article = form.save()
+            return
+    else:
+        form = ArticleForm()
+    context = {
+        'form' : form,
+    }
+    return render(request, 'articles/create.html', context)
