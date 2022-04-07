195c8c72a4d44ef3f768eca90e88c97f9acb8fe9
최수연 <martin0125@naver.com>
2022-04-07 14:17:19 +0900

    create forms file and use ModelForm

---
+++ articles/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from .models import Article
+
+class ArticleForm(forms.ModelForm):
+    title = forms.CharField(
+        label = 'Title:',
+        widget = forms.TextInput(
+            attrs = {
+                'class' : 'title',
+                'placeholder' : 'Enter the title',
+            }
+        ),
+        error_messages={
+        'required' : 'Please enter the title'
+        }
+    )
+    content = forms.CharField(
+        label = 'Contents:',
+        widget = forms.Textarea(
+            attrs = {
+                'class' : 'content',
+                'placeholder' : 'Enter the contents',
+                'rows' : 10,
+                'cols' : 50,
+            }
+        ),
+        error_messages={
+            'required' : 'Please enter the content'
+        }
+    )
